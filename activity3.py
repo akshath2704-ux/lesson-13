88d95a28d037b787d8480cd2f83b2af5e0dd6b9b
akshath2704-ux <akshath2704@gmail.com>
2025-12-10 18:59:20 +0800

    initial commit

--- activity3.py
+++ activity3.py
@@ -13,4 +13,13 @@ for i in range(1,halfdiamrow+1):
         print(end=str(num))
         num=num+1
     print()
-space=1   
+space=1
+for i in range(1,halfdiamrow):
+    for j in range(1,space+1):
+        print(end=" ")
+    space=space+1
+    num=1
+    for j in range(1,2*(halfdiamrow-i)):
+        print(end=str(num))
+        num=num+1
+    print()
